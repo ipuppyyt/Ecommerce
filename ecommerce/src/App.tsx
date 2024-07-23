f663c9925eb4ad16e1ae178b67caf19a666e6c2e
Geonobins <geonobins88@gmail.com>
2024-07-23 18:01:51 +0530

    commit

---
+++ ecommerce/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-4x1 text-blue-500'>Ecommerce Project</h1>
+    </>
+  )
+}
+
+export default App
